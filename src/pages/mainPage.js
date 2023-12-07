be6332d10bd013647b9a93e55ba0553767301dbc
gaobingbing <1019178691@qq.com>
2023-12-07 10:32:22 +0800

    首页

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-import { Button } from 'antd';
+import { Image } from 'antd';
+import aweGif from '../asset/img/awe.gif';
+import anGif from '../asset/img/3d.gif';
+import an2Gif from '../asset/img/3d2.gif'; 
 
 function MainPage() {
 
@@ -11,9 +14,29 @@ function MainPage() {
   }
 
   return (
-    <div>
-      首页
-      <Button onClick={handleClickToMenu}>菜单</Button>
+    <div className='mainPageDiv'>
+      <div style={{display:'flex', alignItems:'center', justifyContent:'center', flexDirection:'column', height:'100%'}}>
+        <Image 
+          src={aweGif} 
+          preview={false}
+        />
+        <div className="button-hand" style={{marginTop:50,position:'relative'}}>
+          <button onClick={handleClickToMenu}>Click to enter!<div className="hands"></div></button>
+          <div style={{position:'absolute', left: '250px', bottom: '-80px'}}>  
+            <Image 
+              style={{width:300}}
+              src={an2Gif} 
+              preview={false}
+            />
+          </div>
+        </div>
+      </div>
+      <div style={{position:'absolute', right: 100, bottom: '10%'}}>
+        <Image 
+          src={anGif} 
+          preview={false}
+        />
+      </div>
     </div>
   );
 }
